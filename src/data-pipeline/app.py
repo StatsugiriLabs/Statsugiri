55b36dbec5b32f87578260bf54f9ef2a8728dec0
kelvinkoon <kkoon@shaw.ca>
2021-12-09 01:23:30 -0800

    Add ladder user and rating getter and formatting scripts

---
+++ src/data-pipeline/app.py
@@ -0,0 +1,22 @@
+""" Application-level logic for facilitating data pipeline """
+import logging
+
+# from data_extractor import DataExtractor
+# from constants import FORMATS
+
+
+def init_logging(level: int):
+    """Initialize logging configurations"""
+    logging.basicConfig(level=level)
+
+
+def main():
+    """Main function"""
+    init_logging(logging.INFO)
+    logging.info("Initializing data pipeline...")
+    # data_extractor = DataExtractor(FORMATS)
+    # data_extractor.extract_info("gen8vgc2021series10")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/data-pipeline/constants.py
@@ -0,0 +1,4 @@
+""" Constants shared between modules """
+FORMATS = ["gen8vgc2021series10", "ou"]
+NUM_TEAMS = 100
+MAX_USERS = 500
