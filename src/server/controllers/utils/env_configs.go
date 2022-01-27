307aab2420c146f4cbffbc15b050696cd52312fd
kelvinkoon <kkoon@shaw.ca>
2022-01-26 22:27:10 -0800

    Make MongoURI default conform to format

--- src/server/controllers/utils/env_configs.go
+++ src/server/controllers/utils/env_configs.go
@@ -12,7 +12,7 @@ func getEnv(key, fallback string) string {
 	return value
 }
 
-var MongoUri string = getEnv("MONGOURI", "")
+var MongoUri string = getEnv("MONGOURI", "mongodb+srv://user:PASSWORD@cluster.a1aa1.mongodb.net/cluster?retryWrites=true&w=majority")
 var Env string = getEnv("ENV", "DEV")
 var DbClusterName string = getEnv("DB_CLUSTER_NAME", "")
 var PokemonTeamsSnapshotsCollection string = getEnv("POKEMON_TEAMS_SNAPSHOTS_COLLECTION", "")
