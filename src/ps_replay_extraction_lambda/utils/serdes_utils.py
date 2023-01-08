d51b3160641e7a9938654d7fcd50b29855bdddd8
kelvinkoon <kkoon@shaw.ca>
2023-01-07 23:32:21 -0800

    Serialize snapshot to JSON for Step Function

---
+++ src/ps_replay_extraction_lambda/utils/serdes_utils.py
@@ -0,0 +1,20 @@
+# https://stackoverflow.com/questions/1036409/recursively-convert-python-object-graph-to-dictionary
+def to_dict(obj: any, classkey=None):
+    if isinstance(obj, dict):
+        data = {}
+        for (k, v) in obj.items():
+            data[k] = to_dict(v, classkey)
+        return data
+    elif hasattr(obj, "_ast"):
+        return to_dict(obj._ast())
+    elif hasattr(obj, "__iter__") and not isinstance(obj, str):
+        return [to_dict(v, classkey) for v in obj]
+    elif hasattr(obj, "__dict__"):
+        data = dict([(key, to_dict(value, classkey)) 
+            for key, value in obj.__dict__.items() 
+            if not callable(value) and not key.startswith('_')])
+        if classkey is not None and hasattr(obj, "__class__"):
+            data[classkey] = obj.__class__.__name__
+        return data
+    else:
+        return obj
