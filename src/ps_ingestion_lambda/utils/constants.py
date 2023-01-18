b55a4a47f7ade6d31b23e456f1c59ec01bf5c03f
kelvinkoon <kkoon@shaw.ca>
2023-01-18 01:18:12 -0500

    Fix Twitter client writer with correct client name

--- src/ps_ingestion_lambda/utils/constants.py
+++ src/ps_ingestion_lambda/utils/constants.py
@@ -1,8 +1,7 @@
-import os
-
 CURR_VGC_FORMAT = "gen9vgc2023series1"
 VALID_FORMATS = [
     CURR_VGC_FORMAT,
+    "gen9vgc2023series2",
     "gen9ou",
 ]
 
@@ -13,9 +12,3 @@ TEAM_SIZE = 6
 REPLAY_BASE_URL = "https://replay.pokemonshowdown.com/"
 REPLAY_SEARCH_BASE_URL = "https://replay.pokemonshowdown.com/search/?output=html&user="
 REQUEST_TIMEOUT = 2  # [seconds]
-
-TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-TWITTER_API_KEY = os.environ.get("TWITTER_API_KEY")
-TWITTER_API_KEY_SECRET = os.environ.get("TWITTER_API_KEY_SECRET")
-TWITTER_DISPLAY_NAME = os.environ.get("TWITTER_DISPLAY_NAME")
