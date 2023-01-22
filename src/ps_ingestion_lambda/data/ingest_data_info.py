a4d2c165b3b767c432ae1126d06ff62cc39dce19
kelvinkoon <kkoon@shaw.ca>
2023-01-22 13:53:34 -0500

    Deprecate previous ingestion pipeline, re-assign ingestion to 100 teams

--- src/ps_ingestion_lambda/data/ingest_data_info.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class IngestDataInfo:
-    snapshot_date: str
-    num_users_to_pull: int
--- src/ps_ingestion_lambda/utils/base_logger.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logger = logging.getLogger()
-logging.basicConfig(format="%(asctime)s - %(message)s")
-logger.setLevel(logging.INFO)
