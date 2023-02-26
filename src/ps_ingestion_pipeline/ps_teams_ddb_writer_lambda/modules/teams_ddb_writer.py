2831d9e87ad7393f4b964a288305bd538c79db7e
kelvinkoon <kkoon@shaw.ca>
2023-02-26 18:09:59 -0500

    Add exception handling for DDB writes

--- src/ps_ingestion_pipeline/ps_teams_ddb_writer_lambda/modules/teams_ddb_writer.py
+++ src/ps_ingestion_pipeline/ps_teams_ddb_writer_lambda/modules/teams_ddb_writer.py
@@ -11,6 +11,7 @@ from utils.constants import (
     ID_EVENT_ARG,
     REPLAY_UPLOAD_DATE_EVENT_ARG,
 )
+from utils.errors import DdbTeamsWriteException
 
 PK_DDB_KEY = "pk"
 SK_DDB_KEY = "sk"
@@ -41,35 +42,38 @@ class DdbTeamsWriter:
         format_id = team_snapshot_payload[FORMAT_ID_EVENT_ARG]
         team_list = team_snapshot_payload[TEAM_LIST_EVENT_ARG]
 
-        for team_info in team_list:
-            pk_id = str(uuid.uuid4())
-            date_field = DATE_FIELD_PREFIX + SK_DELIMITER + snapshot_date
-            lowercase_team_list = [
-                pkmn.lower() for pkmn in team_info[PKMN_TEAM_EVENT_ARG]
-            ]
+        try:
+            for team_info in team_list:
+                pk_id = str(uuid.uuid4())
+                date_field = DATE_FIELD_PREFIX + SK_DELIMITER + snapshot_date
+                lowercase_team_list = [
+                    pkmn.lower() for pkmn in team_info[PKMN_TEAM_EVENT_ARG]
+                ]
 
-            # Write primary team item
-            primary_team_item = {
-                PK_DDB_KEY: {"S": pk_id},
-                SK_DDB_KEY: {"S": date_field},
-                SNAPSHOT_DATE_DDB_KEY: {"S": snapshot_date},
-                FORMAT_ID_DDB_KEY: {"S": format_id},
-                REPLAY_ID_DDB_KEY: {"S": team_info[ID_EVENT_ARG]},
-                PKMN_TEAM_DDB_KEY: {"SS": lowercase_team_list},
-                RATING_DDB_KEY: {"N": str(team_info[RATING_EVENT_ARG])},
-                REPLAY_UPLOAD_DATE_DDB_KEY: {
-                    "S": team_info[REPLAY_UPLOAD_DATE_EVENT_ARG]
-                },
-            }
-            self.teams_ddb_client.put_item(primary_team_item)
-
-            # Write pkmn_team items
-            for pkmn in lowercase_team_list:
-                pkmn_field = PKMN_FIELD_PREFIX + SK_DELIMITER + pkmn
-                pkmn_team_item = {
+                # Write primary team item
+                primary_team_item = {
                     PK_DDB_KEY: {"S": pk_id},
-                    SK_DDB_KEY: {"S": pkmn_field},
+                    SK_DDB_KEY: {"S": date_field},
+                    SNAPSHOT_DATE_DDB_KEY: {"S": snapshot_date},
+                    FORMAT_ID_DDB_KEY: {"S": format_id},
+                    REPLAY_ID_DDB_KEY: {"S": team_info[ID_EVENT_ARG]},
+                    PKMN_TEAM_DDB_KEY: {"SS": lowercase_team_list},
+                    RATING_DDB_KEY: {"N": str(team_info[RATING_EVENT_ARG])},
+                    REPLAY_UPLOAD_DATE_DDB_KEY: {
+                        "S": team_info[REPLAY_UPLOAD_DATE_EVENT_ARG]
+                    },
                 }
-                self.teams_ddb_client.put_item(pkmn_team_item)
+                self.teams_ddb_client.put_item(primary_team_item)
 
-        return True
+                # Write pkmn_team items for efficient look-up
+                for pkmn in lowercase_team_list:
+                    pkmn_field = PKMN_FIELD_PREFIX + SK_DELIMITER + pkmn
+                    pkmn_team_item = {
+                        PK_DDB_KEY: {"S": pk_id},
+                        SK_DDB_KEY: {"S": pkmn_field},
+                    }
+                    self.teams_ddb_client.put_item(pkmn_team_item)
+
+            return True
+        except Exception as e:
+            raise DdbTeamsWriteException("Error writing to table:{err}".format(err=str(e)))
