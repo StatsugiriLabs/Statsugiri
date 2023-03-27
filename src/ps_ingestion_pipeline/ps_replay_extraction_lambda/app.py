41ffa5948d4c7f14469cee6324d3327c69975322
kelvinkoon <kkoon@shaw.ca>
2023-03-27 00:41:31 -0400

    Rename PS teams Lambda service, fix type comparison

--- src/ps_ingestion_pipeline/ps_replay_extraction_lambda/app.py
+++ src/ps_ingestion_pipeline/ps_replay_extraction_lambda/app.py
@@ -44,7 +44,7 @@ def lambda_handler(event: LambdaDict, context: LambdaContext) -> dict:
     ingest_config = PsIngestConfig(
         convert_unix_timestamp_to_str(int(time.time())),
         format_to_search,
-        NUM_USERS_TO_PULL,
+        int(NUM_USERS_TO_PULL),
     )
     ps_replay_extraction_client = PsReplayExtractionClient(
         ReplayExtractor(LadderRetriever(), ingest_config)
