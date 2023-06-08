bc2a09586fffa1101ad02b9a5cfc217ea030f1ca
kelvinkoon <kkoon@shaw.ca>
2023-06-08 14:20:15 -0700

    Add additional skeleton code for client

--- src/client/src/components/ps_teams/PsTeamItem.tsx
+++ src/client/src/components/ps_teams/PsTeamItem.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import { PsTeam } from "../../types";
+import { PsTeam } from "../../../types";
 import Image from "next/image";
 import { getPkmnSpriteName } from "@/utils/getPkmnSpriteName";
 
@@ -11,6 +11,18 @@ type Props = {
 const PsTeamItem: FunctionComponent<Props> = ({ rank, team }) => {
     return (
         <div>
+            <a
+                href="#"
+                className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Noteworthy technology acquisitions 2021
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Here are the biggest enterprise technology acquisitions of
+                    2021 so far, in reverse chronological order.
+                </p>
+            </a>
             <p>{"Rank #" + rank}</p>
             {team.pkmn_team.map((pkmn, index) => {
                 return (
---
+++ src/client/src/components/shared/Layout.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent, ReactNode } from "react";
+import Meta from "./Meta";
+
+interface Props {
+    children?: ReactNode;
+}
+
+const Layout: FunctionComponent<Props> = (props) => {
+    return (
+        <>
+            <Meta />
+            <p>Nav</p>
+            <div className="container mx-auto flex items-center">
+                <main className={""}>
+                    <p>Header</p>
+                    {props.children}
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default Layout;
