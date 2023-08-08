0a96f962537140fdd53057501be22cbfa70331a5
kelvinkoon <kkoon@shaw.ca>
2023-08-07 22:42:11 -0400

    Add Tailwind plugin to remediate Vercel deploy issues

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
@@ -14,5 +14,5 @@ module.exports = {
             },
         },
     },
-    plugins: [],
+    plugins: ["tailwindcss"],
 };
